80e362d6f0eda23bb1a7313d4ccd1ec115c9e990
김지혜 <jkim@ui-lab.co.kr>
2024-01-04 00:31:25 +0900

    aos 설치

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueTyperPlugin from 'vue-typer'
+import AOS from 'aos';
+import "aos/dist/aos.css";
 // CommonJS
 // var VueTyperPlugin = require('vue-typer').default
 // Global
@@ -13,6 +15,9 @@ Vue.use(VueTyperPlugin)
 Vue.config.productionTip = false
 
 new Vue({
+  created(){
+    AOS.init();
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
