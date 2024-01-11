81bb89236df8f85ce959cfa5341fe4219590e858
김지혜 <k_jihye92@naver.com>
2024-01-11 15:52:46 +0900

    calendar view 새로운 레퍼지토리로 이관

--- src/router/index.js
+++ src/router/index.js
@@ -16,15 +16,6 @@ const routes = [
     name: 'workView',
     component: WorkView
   },
-  {
-    path: '/Calendar',
-    name: 'CalendarView',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/CalendarView.vue')
-  },
-
 ]
 
 const router = new VueRouter({
