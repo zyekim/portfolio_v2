67439a6d9cf263d0f76d94a75bee6deaeaf35b1c
김지혜 <k_jihye92@naver.com>
2024-02-14 07:54:52 +0900

    Resume 파일추가

---
+++ src/views/ResumeView.vue
@@ -0,0 +1,380 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="img-wrap">
+        <img src="@/assets/images/profile_img.jpg" alt="이미지" />
+      </div>
+      <div class="section section--top">
+        <div class="section__item">
+          <h1>FE개발자 김지혜</h1>
+          <p class="intro-text">
+            <b>Think Positive!</b> <br />언제나 긍정의 에너지로 앞으로 나아가는
+            <br />
+            주니어 프론트엔드 개발자 김지혜입니다:)
+          </p>
+          <ul class="dot-style mt20">
+            <li>협업을 위해 배려하고 소통하며! 성장하기 위해 노력합니다.</li>
+            <li>
+              새로운 도전을 즐기며 웹 기술의 끊임없는 발전에 흥미를 느낍니다.
+            </li>
+            <li>
+              다양한 프로젝트 경험으로 효율적인 코드 작성과 협업의 중요성을
+              압니다.
+            </li>
+          </ul>
+        </div>
+        <div class="section__item" style="align-self: flex-end">
+          <ul>
+            <li>✉️ k_jihye92@naver.com</li>
+            <li>📱 +82 010-9386-0586</li>
+            <li>
+              📒 Portfolio :
+              <a href="https://zyekim.github.io/#/"
+                >https://zyekim.github.io/#/</a
+              >
+            </li>
+            <li>
+              📝 Blog :
+              <a href="https://velog.io/@k_jihye92/posts"
+                >https://velog.io/@k_jihye92/posts</a
+              >
+            </li>
+            <li>
+              🐙 Github:
+              <a href="https://github.com/zyekim">https://github.com/zyekim</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <span class="divider"></span>
+      <div class="section">
+        <div class="section__item">
+          <h2>Project</h2>
+          <div class="period-wrap">
+            <div class="period-wrap__period">
+              <span class="caption"> 2022.01 - 2022.08 </span>
+            </div>
+            <div class="period-wrap__details">
+              <h3>페이퍼리스<a href="https://paperless.onl/">🔗</a></h3>
+              <p>프론트엔드 개발 40% & 웹퍼블리싱 100%</p>
+              <ul>
+                <li>
+                  신규 서비스(메인/마케팅/관리자 3개 페이지) 구축/ 기획 백엔드
+                  협업
+                </li>
+                <li>웹퍼블리싱 100% / 프론트엔드 개발 30%</li>
+                <li>마케팅웹 화면 SEO 최적화 위해 nuxt.js 활용</li>
+                <li>메인 서비스 화면 폴더 구조 수립 (router)</li>
+                <li>메인 서비스 내 정보 수정 화면 개발</li>
+                <li>
+                  [Vuetify](https://vuetifyjs.com/en/) 기반 컴포넌트 개발
+                  (dialog/tooltip/toast)
+                </li>
+                <li>Html to pdf 개발 경험</li>
+              </ul>
+            </div>
+          </div>
+          <h3>
+            SK텔링크 홈페이지 리뉴얼 (2개월)**
+            [🔗](https://www.sktelink.com/main)
+          </h3>
+          <ul>
+            <li>Vue 기반으로 마이그레이션 프로젝트</li>
+            <li>Footer 개발</li>
+          </ul>
+          <h3>아이스크림 에듀 ‘국어 문해력이 답이다’ (5개월)</h3>
+          <ul>
+            <li>태블릿 app vue 기반 웹 퍼블리싱 100% (vue, html, scss)</li>
+            <li>Button component 개발</li>
+          </ul>
+        </div>
+      </div>
+      <span class="divider"></span>
+      <div class="section">
+        <div class="section__item">
+          <h2>Experience</h2>
+          <h3>유아이랩</h3>
+          <span>프론트엔드개발자 / 웹 퍼블리셔</span>
+        </div>
+        <div class="section__item">
+          <h2>Skill</h2>
+          <ul>
+            <li><span>Project</span> Management Notion, Confluence</li>
+            <li><span>Office</span> MS Office Suite, Slack, Zoom</li>
+            <li>
+              <span>UX/UI</span> Zepline, Figma, Adobe Photoshop, Illustrator,
+              XD
+            </li>
+            <li><span>Dev</span> HTML5, CSS3, Scss, Javascript, Vue, React</li>
+            <li><span>Others</span> SourceTree, Procreate</li>
+          </ul>
+        </div>
+      </div>
+      <span class="divider"></span>
+      <span class="divider"></span>
+      <div class="section">
+        <div class="section__item">
+          <h2>Training</h2>
+          <ul>
+            <li>
+              [웹코딩]UIUX반응형(REACT,javascript),프론트엔드 개발자 2023/04 -
+              2023/05 : React 기초과정 수강
+            </li>
+            <li>
+              UI/UX 반응형 웹디자인 웹퍼블리셔 국비교육 -
+              HTML,CSS,jQuery,javascript, vue.js 등의 기초지식부터 습득하고,
+              실무위주의 프로 젝트들을 수행하면서 완성도 있는 페이지를 구현 -
+              성적우수상 수상
+            </li>
+            <li>
+              빅데이터 소셜마케팅 전문인력 양성과정 - 빅데이터 활용하여
+              인사이트를 도출해내는 여러 프로젝트 수행 - 공모전 장려상 수상
+            </li>
+          </ul>
+        </div>
+      </div>
+      <span class="divider"></span>
+      <div class="section">
+        <div class="section__item">
+          <h2>Education</h2>
+          <ul>
+            <li>서경대학교 인문대학 국제비즈니스학부 졸업</li>
+            <li>염광고등학교 졸업</li>
+          </ul>
+        </div>
+      </div>
+      <span class="divider"></span>
+      <div class="section">
+        <div class="section__item">
+          <h2>Certification</h2>
+          <ul>
+            <li>2020 웹디자인기능사 취득</li>
+          </ul>
+        </div>
+        <div class="section__item">
+          <h2>Languages</h2>
+          <ul>
+            <li><span>2016</span>Toeic 860</li>
+            <li><span>2016</span>Toeic speaking lv.6</li>
+            <li><span>2016</span>HSK 6급</li>
+          </ul>
+        </div>
+      </div>
+      <span class="divider"></span>
+      <div class="section">
+        <div class="section__item">
+          <h2>Cover Letter</h2>
+          <h3>지원동기/입사후 포부 - 끊임없는 학습과 도전</h3>
+          <div class="cv-text">
+            약 2년간 웹 퍼블리셔로서 업무하면서, 때때로 개발팀의 산출물이 기획
+            의도나 디자인 요소가 정확히 반영되지 않는 일들이 있었습니다.
+            <br />
+            더 완성도 높은 웹 서비스를 제공하기 위해서 단순 모크업 페이지를 넘어
+            직접 고객의 니즈를 파악하여 고객이 원하는 화면을 직접 개발해보고
+            싶은 열의가 생겼습니다.
+            <br /><br />
+            페이버리스 전자 계약 서비스 런칭 프로젝트 때의 일입니다. 마케팅/메인
+            웹 화면 퍼블리싱을 전담하면서도, 프론트엔트 개발자로서의 역량을
+            키우기 위해 직접 자원하여 화면 개발에 참여했습니다. 데이터를 수신,
+            비교하고 송신하는 과정이 예상했던 것보다 복잡했지만, 문제 해결을
+            위해 퇴근 후 ‘Vue’ ‘Javascript’ 강의를 꾸준히 수강했고, 코딩테스트
+            가능한 플랫폼을 활용하여, 개발자 역량을 강화해왔습니다. 그 결과 '내
+            정보 수정'화면 개발을 100% 전담하여 무사히 마칠 수 있었습니다.
+            <br /><br />
+            어떤 프로젝트를 수행하더라도 개발하기 편한 코드가 아닌 항상 사용자
+            친화적인 프로덕트를 구현하기 위한 코드를 고민하는 것이 중요하다고
+            생각합니다. <br />
+            끊임없는 배움과 도전을 통해 원클릭으로 고객이 원하는 정확한 정보를
+            제공하는 직관적인 화면을 구현하는 개발자가 되고 싶습니다.
+          </div>
+          <h3>핵심 역량 - 시각화로 정확하고 명료한 '커뮤니케이션'</h3>
+          <div class="cv-text">
+            프로젝트를 성공적으로 수행함에 있어서 다양한 분야의 팀과 적극적인
+            소통과 협업 관건입니다. 1년 동안 '교원 차세대 영업 관리 시스템 구축
+            프로젝트'를 진행하며, 인도 GDC와의 협업을 통해 대규모 페이지를
+            제작해야 했습니다. 이 프로젝트는 고객 니즈에 따른 지속적인 기획 및
+            디자인 변경으로 인해 공통 컴포넌트의 형태가 빈번히 수정되어야 하는
+            복잡한 상황이었습니다. 더불어, 변경이 필요한 상황은 계속해서
+            발생하는 반면, 관리자와 통역사를 통해 GDC 작업자들과의 소통 과정에서
+            요청사항이 누락되거나 실시간으로 업데이트되지 않아, 페이지 완성도
+            측면에 문제가 발생해 지연돼서 오픈 기한을 맞추지 못할 위기에
+            처해있었습니다.이를 해결하기 위해 외부적으로는(인도 개발자에게는)
+            변경이 필요할 경우를 대비해 공통 레이아웃과 컴포넌트 등 하여 직접
+            CSS와 HTML 구조를 제작해 제공했습니다. 더불어, 페이지 내 적재적소에
+            배치하도록 작업 가이드를 메뉴얼화하고 지속적인 검수를 통해 표준
+            레이아웃을 벗어는 공통된 문제점을 찾아내 피드백을 제공했습니다.
+            실시간 의사소통을 위해 일정 시간을 정해두고, 제플린 맨션 창으로
+            소통창구를 열어 상호 궁금한 점을 바로바로 확인하였고, 컨플루언스를
+            활용하여 업데이트 내용을 공유함으로써 즉각 대응이 가능한 체계를
+            구축했습니다. 또한, 내부적으로는(내부 기획자에게는) 웹 기획서를
+            기반으로 페이지 제작 시, 버튼 및 기타 공통 컴포넌트를 나타내는 시각
+            자료를 통일해 제공하도록 소통하였고, 상호 언어적 한계에 따른 문제를
+            해소할 수 있었습니다. 이러한 노력으로 3주 지연이 불가피한 상황을
+            극복하고 프로젝트를 기간 내에 마무리했으며, 고객사로부터는 기존
+            GDC가 참여한 프로젝트 중 가장 완성도 높은 웹 퍼블리싱 화면으로
+            평가받았습니다. 문제 발생 시 원인을 논리적으로 파악하고, 팀원들과
+            원활한 소통을 통해 시각화된 자료를 제공하는 등 문제를 효과적이고
+            체계적으로 해결하는 능력을 갖고 있습니다. 앞으로도 예상치 못한
+            문제에 도전하고 대처하고, 적극적으로 의사소통하고 협업하여 더 나은
+            결과물을 위해 지속적으로 발전하는 개발자로 성장하고자 합니다.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // import VueHtml2pdf from "vue-html2pdf";
+  export default {
+    name: "ResumeView",
+    // components: {
+    //   VueHtml2pdf,
+    // },
+    // methods: {
+    //   generateReport() {
+    //     this.$refs.html2Pdf.generatePdf();
+    //   },
+    // },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap");
+  @page {
+    size: letter;
+    margin: 1cm;
+    bleed: 3mm;
+  }
+
+  * {
+    text-align: left;
+    font-family: "Noto Sans KR", sans-serif;
+  }
+
+  .intro-text {
+    position: relative;
+    padding-left: 14px;
+    font-weight: 500;
+    line-height: 1.56;
+    &:before {
+      position: absolute;
+      left: 2px;
+      top: 0;
+      bottom: 0;
+      width: 4px;
+      background-color: #222;
+      content: "";
+    }
+  }
+  h1 {
+    font-size: 33px;
+    font-weight: bold;
+  }
+
+  h2 {
+    margin-bottom: 18px;
+    font-size: 25px;
+    font-weight: bold;
+    color: #99ad5e;
+  }
+
+  h3 {
+    // margin-top: 10px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  p {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.43;
+  }
+
+  span.caption {
+    font-weight: normal;
+    font-style: italic;
+    font-size: 15px;
+    color: #bbb;
+  }
+
+  span.divider {
+    display: block;
+    width: 100%;
+    height: 1px;
+    background-color: #efefef;
+  }
+
+  .img-wrap {
+    width: 200px;
+    height: 185px;
+    img {
+      // width: 100%;
+      // height: 100%;
+    }
+  }
+
+  .container {
+    margin: 0 auto;
+    padding: 30px;
+    width: 100%;
+    height: 100%;
+    max-width: 1280px;
+  }
+
+  ul li {
+    &:not(:first-of-type) {
+      padding-top: 4px;
+    }
+  }
+
+  .section {
+    padding: 40px 0px;
+    display: flex;
+    column-gap: 50px;
+    width: 100%;
+    &__item {
+      flex: 1 1 45%;
+      word-break: keep-all;
+    }
+    &--top {
+      padding-top: 20px;
+      column-gap: 80px;
+    }
+  }
+
+  .cv-text {
+    margin-top: 18px;
+    line-height: 1.43;
+    & + h3 {
+      margin-top: 30px;
+    }
+  }
+
+  .period-wrap {
+    display: flex;
+    column-gap: 50px;
+    &__period {
+      flex: 0 0 200px;
+    }
+    &__details {
+      flex: 1 1 80%;
+    }
+  }
+
+  .dot-style {
+    li {
+      white-space: normal;
+      &:before {
+        margin-right: 8px;
+        display: inline-block;
+        vertical-align: 4px;
+        content: "";
+        width: 5px;
+        height: 5px;
+        background-color: #222;
+        border-radius: 100%;
+      }
+    }
+  }
+</style>
