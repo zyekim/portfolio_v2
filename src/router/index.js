6665e8d39350c8a1da6b63e086e0d37e951013c3
김지혜 <k_jihye92@naver.com>
2026-04-26 07:48:36 +0900

    remove resume router

--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,48 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import HomeFE from '../views/HomeFE.vue'
-import HomePub from '../views/HomePub.vue'
-import WorkView from '../views/WorkView.vue'
-import ResumeView from '../views/ResumeView.vue'
-import PubResumeView from '../views/PubResumeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import HomeFE from "../views/HomeFE.vue";
+import HomePub from "../views/HomePub.vue";
+import WorkView from "../views/WorkView.vue";
+// import ResumeView from "../views/ResumeView.vue";
+import PubResumeView from "../views/PubResumeView.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: HomeView,
     children: [
       {
-        path: '/',
-        component: HomeFE
+        path: "/",
+        component: HomeFE,
       },
       {
-        path: '/publish',
-        component: HomePub
+        path: "/publish",
+        component: HomePub,
       },
-    ]
+    ],
   },
   {
-    path: '/work',
-    name: 'WorkView',
-    component: WorkView
+    path: "/work",
+    name: "WorkView",
+    component: WorkView,
   },
+  // {
+  //   path: '/resume',
+  //   name: 'ResumeView',
+  //   component: ResumeView
+  // },
   {
-    path: '/resume',
-    name: 'ResumeView',
-    component: ResumeView
+    path: "/resume/pub",
+    name: "PubResumeView",
+    component: PubResumeView,
   },
-  {
-    path: '/resume/pub',
-    name: 'PubResumeView',
-    component: PubResumeView
-  },
-]
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
